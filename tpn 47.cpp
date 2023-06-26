46045b3c6906d06219c3923385eb93872f1d7009
SaraWillersLohr <134223842+SaraWillersLohr@users.noreply.github.com>
2023-06-26 14:14:34 -0300

    Add files via upload

---
+++ tpn 47.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+bool EsAbundante (int a);
+
+
+int main()
+{
+  string  msj="NO ES ABUNDANTE";
+  int n1,i;
+  
+    cout << "Ingrese un número" << endl;
+    cin >> n1;
+    for(i=1;i<=n1;i++)
+    {
+    
+    if( EsAbundante (i))
+    {
+     cout<<i<<"-";
+    }
+    }
+    
+  
+    return 0;
+}
+bool EsAbundante (int a)
+{
+  int n=0;
+  bool flag=false;
+  for (int i = 1; i <= a / 2; i++)
+    {
+      if (a % i == 0)
+	{
+	  n =n+i;
+	}
+    }
+    
+    if (n>a)
+    {
+        flag=true;
+    }
+return flag;
+}
