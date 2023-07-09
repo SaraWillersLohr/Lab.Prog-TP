ef7913b25bf31ed58a4e05918093c0c15ef35f51
SaraWillersLohr <134223842+SaraWillersLohr@users.noreply.github.com>
2023-07-09 15:21:17 -0300

    Add files via upload

---
+++ TP 51.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h> 
+using namespace std;
+
+void ImprimirCartel (int n); 
+int main() {
+
+int n; cin>>n;
+
+ImprimirCartel(n);
+
+return 0;
+
+}
+
+void ImprimirCartel (int n){
+
+int i;
+
+for (i=0;i<n;i++){
+
+cout<<"hola mundo "<<endl;
+
+}
+
+}
