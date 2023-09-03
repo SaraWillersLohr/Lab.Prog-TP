cdfb9f8803581cf6dd80a9734246d3390b21115c
SaraWillersLohr <134223842+SaraWillersLohr@users.noreply.github.com>
2023-09-03 13:19:27 -0300

    Add files via upload

---
+++ tp 68.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+ 
+string InsertarPunto (string frase);
+
+ 
+int
+main () 
+{
+  
+string f;
+  
+ 
+cout << "Ingrese una cadena: " << endl;
+  
+getline (cin, f);
+  
+ 
+cout << InsertarPunto (f) << endl;
+  
+ 
+return 0;
+
+}
+
+
+ 
+string InsertarPunto (string frase) 
+{
+  
+string resultado;
+  
+int contador = 0;
+  
+ 
+for (int i = 0; i < frase.size (); i++)
+    
+    {
+      
+resultado += frase[i];
+      
+contador++;
+      
+ 
+if (contador == 3 and i != frase.size () - 1)
+	
+	{
+	  
+resultado += '.';
+	  
+contador = 0;
+	
+}
+    
+}
+  
+ 
+return resultado;
+
+}
+
+
